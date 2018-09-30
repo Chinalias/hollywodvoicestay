eeee5a3590c8db0467b0f0e1d0deb2cba9f4c780
bakutkisa241 <35742714+bakutkisa241@users.noreply.github.com>
2018-10-01 00:14:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,30 @@ client.on('ready', () => {
   console.log('---------------')
 });
 
+
+const YTDL = require('ytdl-core');
+const nodeopus = require('node-opus');
+const ffmpeg = require('ffmpeg');
+var servers = {};
+function play(connection, message, args) {
+  var server = servers[message.guild.id];
+  server.dispatcher = connection.playStream(YTDL(args[0]), {filter: "audioonly"});
+  server.queue.shift();
+  server.dispatcher.on("end", function() {
+    if (server.queue[0]) play(connection, message);
+    else connection.disconnect();
+  });
+}
+
+
+client.on('message', message =>{
+  if(message.content.startsWith('Jjoin')){
+    const voiceChannel = message.member.voiceChannel
+    voiceChannel.join();
+    message.channel.send("Connected to voice channel")
+}})
+
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
